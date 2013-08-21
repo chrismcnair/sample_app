82934380e09b29aab5d28304e7ceecca244a55cb
Chris McNair <chrismcnair83@gmail.com>
2013-08-20 20:46:33 -0700

    Add route, controller action, and about template and also rspec test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -21,7 +21,7 @@ describe "StaticPages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit 'static_pages/about'
+      visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
   end
